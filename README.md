674657eaeb6af2c4ee29fca330d6ae66bb886bc9
Robert <robxyz567@gmail.com>
2021-12-11 18:29:41 +0100

    full working version

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 This is a typing speed test app written in Python version 3.9.
-Working version, not final. 
 'Tkinter' was used.
 Owner of this code is 'robxyz567@gmail.com'.
 English words were taken from: "https://www.ef.com/wwen/english-resources/english-vocabulary/top-3000-words/"
+Some colors from https://colorhunt.co/
--- main.py
+++ main.py
@@ -3,11 +3,31 @@ import random
 from datetime import datetime
 
 
-def starting_count_down(count):
+def start_count_down(count):
+    global text, total_words, total_chars, word_counter, index
 
-    time_label.config(text=f"Time do start: {count}", fg='#FF6D6D')
+    if count == 3:
+
+        word_counter = 0
+        index = 0
+        total_words = 0
+        total_chars = 0
+
+        words_list = list(open('english_words.txt'))
+        text = []
+        for i in range(100):
+            text.append(random.choice(words_list).replace('\n', ''))
+
+        text_label1.config(text=text[0:1], fg='black')
+        text_label2.config(text=text[1:2], fg='black')
+        text_label3.config(text=text[2:3], fg='black')
+        text_label4.config(text=text[3:4], fg='black')
+        text_label5.config(text=text[4:5], fg='black')
+        text_label6.config(text=text[5:6], fg='black')
+
+    message_label.config(text=f"Time do start: {count}", fg='#FF6D6D')
     if count > 0:
-        window.after(1000, starting_count_down, count-1)
+        window.after(1000, start_count_down, count-1)
     elif count == 0:
         typing()
         typing_count_down(60)
@@ -16,7 +36,8 @@ def starting_count_down(count):
 
 def typing_count_down(seconds):
 
-    time_label.config(text=f"Seconds to end: {seconds}", fg='black')
+    if seconds != 0:
+        message_label.config(text=f"Seconds to end: {seconds}", fg='black')
     if seconds > 0:
         window.after(1000, typing_count_down, seconds-1)
 
@@ -64,16 +85,15 @@ def typing():
 def calculate_result():
     global total_words, total_chars
 
-    result_label.focus()
+    message_label.focus()
     text_entry.delete(0, 'end')
-    time_label.config(text='STOP!', fg='#FF6D6D')
     total_words = index + word_counter
 
     total_chars = 0
     for word in text[:total_words]:
         total_chars += len(word)
 
-    result_label.config(text=f"Words/chars per min: {total_words}/{total_chars}")
+    message_label.config(text=f"Words/chars per min: {total_words}/{total_chars}", fg='#FF6D6D')
 
 
 def save():
@@ -85,18 +105,13 @@ def save():
         with open('results.txt', 'a') as file:
             file.write(f'{now}, Words/chars per min: {total_words}/{total_chars}\n')
     except NameError:
-        time_label.config(text="No results to save")
+        message_label.config(text="No results to save")
 
 
 def quiting():
 
     window.destroy()
 
-
-words_list = list(open('english_words.txt'))
-text = []
-for i in range(100):
-    text.append(random.choice(words_list).replace('\n', ''))
 
 word_counter = 0
 index = 0
@@ -108,32 +123,30 @@ window.config(padx=50, pady=25, bg='#F2EDD7')
 logo_label = Label(height=2, text="TYPING SPEED TEST APP", font=('Arial', 18, "bold"), bg='#F2EDD7')
 logo_label.grid(row=0, column=0, columnspan=6)
 
-text_label1 = Label(text=text[0:1], font=('Arial', 15), bg='#F2EDD7')
+text_label1 = Label(height=2, width=9, font=('Arial', 13), bg='#F2EDD7')
 text_label1.grid(row=2, column=0)
-text_label2 = Label(text=text[1:2], font=('Arial', 15), bg='#F2EDD7')
+text_label2 = Label(height=2, width=9, font=('Arial', 13), bg='#F2EDD7')
 text_label2.grid(row=2, column=1)
-text_label3 = Label(text=text[2:3], font=('Arial', 15), bg='#F2EDD7')
+text_label3 = Label(height=2, width=9, font=('Arial', 13), bg='#F2EDD7')
 text_label3.grid(row=2, column=2)
-text_label4 = Label(text=text[3:4], font=('Arial', 15), bg='#F2EDD7')
+text_label4 = Label(height=2, width=9, font=('Arial', 13), bg='#F2EDD7')
 text_label4.grid(row=2, column=3)
-text_label5 = Label(text=text[4:5], font=('Arial', 15), bg='#F2EDD7')
+text_label5 = Label(height=2, width=9, font=('Arial', 13), bg='#F2EDD7')
 text_label5.grid(row=2, column=4)
-text_label6 = Label(text=text[5:6], font=('Arial', 15), bg='#F2EDD7')
+text_label6 = Label(height=2, width=9, font=('Arial', 13), bg='#F2EDD7')
 text_label6.grid(row=2, column=5)
 
-time_label = Label(height=2, text="Waiting ..", font=('Arial', 15, "bold"), bg='#F2EDD7')
-time_label.grid(row=1, column=3, columnspan=3)
-result_label = Label(height=2, text="Words/chars per min: ..", font=('Arial', 15, "bold"), bg='#F2EDD7')
-result_label.grid(row=5, column=0, columnspan=3)
+message_label = Label(height=2, text="Waiting ...", font=('Arial', 14, "bold"), bg='#F2EDD7')
+message_label.grid(row=1, column=3, columnspan=3)
 
-text_entry = Entry(width=75)
+text_entry = Entry(width=96)
 text_entry.grid(row=3, column=0, columnspan=6)
 
-start_button = Button(height=1, width=6, text="START", font=('Arial', 12, "bold"), bg='#9AE66E', command=lambda: starting_count_down(3))
+start_button = Button(height=1, width=9, text="START", font=('Arial', 12, "bold"), bg='#9AE66E', command=lambda: start_count_down(3))
 start_button.grid(row=1, column=0)
-save_button = Button(height=1, width=6, text="SAVE", font=('Arial', 12, "bold"), bg='#84DFFF', command=save)
+save_button = Button(height=1, width=9, text="SAVE", font=('Arial', 12, "bold"), bg='#84DFFF', command=save)
 save_button.grid(row=1, column=1)
-quit_button = Button(height=1, width=6, text="QUIT", font=('Arial', 12, "bold"), bg='#FF6D6D', command=quiting)
+quit_button = Button(height=1, width=9, text="QUIT", font=('Arial', 12, "bold"), bg='#FF6D6D', command=quiting)
 quit_button.grid(row=1, column=2)
 
 
